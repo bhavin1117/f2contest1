e03f7b40c6120e6d3d87278f418a5cdaf75727a6
bhavin1117 <73589024+bhavin1117@users.noreply.github.com>
2024-01-13 19:42:17 +0530

    f2c1

---
+++ script.js
@@ -0,0 +1,110 @@
+/** @format */
+
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    //Write your code here , just console.log
+        let arr = [
+            { id: 1, name: "john", age: "18", profession: "developer" },
+            { id: 2, name: "jack", age: "20", profession: "developer" },
+            { id: 3, name: "karen", age: "19", profession: "admin" },
+        ];
+    
+        let developerList = arr.map(function(employee) {
+            if (employee.profession === "developer") {
+                return employee;
+            }
+        }).filter(function(item) {
+            return item !== undefined;});
+    
+
+        console.log(developerList);
+    }
+    
+    PrintDeveloperbyMap();
+  
+  function PrintDeveloperbyForEach() {
+    //Write your code here , just console.log
+        let arr = [
+            { id: 1, name: "john", age: "18", profession: "developer" },
+            { id: 2, name: "jack", age: "20", profession: "developer" },
+            { id: 3, name: "karen", age: "19", profession: "admin" },
+        ];
+    
+        let developerList = [];
+    
+        
+        arr.forEach(function(employee) {
+            if (employee.profession === "developer") {
+                developerList.push(employee);
+            }
+        });
+    
+        console.log(developerList);
+    }
+    PrintDeveloperbyForEach();
+  
+  function addData() {
+    //Write your code here, just console.log
+        let arr = [
+            { id: 1, name: "john", age: "18", profession: "developer" },
+            { id: 2, name: "jack", age: "20", profession: "developer" },
+            { id: 3, name: "karen", age: "19", profession: "admin" },
+        ];
+    
+        let newEmployee = {
+            id: 4,
+            name: "susan",
+            age: "20",
+            profession: "intern"
+        };
+    
+        arr.push(newEmployee);
+    
+       
+        console.log(arr);
+    }
+    addData();
+  
+  function removeAdmin() {
+    //Write your code here, just console.log
+        let arr = [
+            { id: 1, name: "john", age: "18", profession: "developer" },
+            { id: 2, name: "jack", age: "20", profession: "developer" },
+            { id: 3, name: "karen", age: "19", profession: "admin" },
+        ];
+
+            let updatedArr = arr.filter(function(person) {
+                return person.profession !== "admin";
+            });
+
+            console.log(updatedArr);
+        }
+    
+    removeAdmin();
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+
+        let arr = [
+                { id: 1, name: "john", age: "18", profession: "developer" },
+                { id: 2, name: "jack", age: "20", profession: "developer" },
+                { id: 3, name: "karen", age: "19", profession: "admin" },  
+            ];
+     
+        let newArr = [
+            { id: 4, name: "rajesh", age: "21", profession: "designer" },
+            { id: 5, name: "ramnlal", age: "22", profession: "developer" },
+            { id: 6, name: "jignesh", age: "23", profession: "admin" },
+        ];
+       
+        let combinedArr = arr.concat(newArr);
+        
+        console.log(combinedArr);
+     }
+     
+     concatenateArray();
